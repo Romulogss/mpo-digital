df34571ce5afa693c2e2edcf4e769f179f5c08a3
Rômulo Sá <romulo.gabss@gmail.com>
2023-08-19 18:41:51 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mpo-digital',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
